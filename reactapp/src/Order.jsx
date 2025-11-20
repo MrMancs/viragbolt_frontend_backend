7f24d575b56fa9b669d12bdc36d68dc86e837d46
MrMancs <marcell.szatmari2007@gmail.com>
2025-11-20 10:33:20 +0100

    Fix footer on Orders

--- reactapp/src/Order.jsx
+++ reactapp/src/Order.jsx
@@ -49,20 +49,20 @@ export default function Order() {
       </main>
       <footer className="container">
         <div className="row">
-          <div className="col-md-4 col-lg-4">
+          <div className="col-md-4 col-lg-2">
             <h3>Nyitvatartás:</h3>
           </div>
-          <div className="col-md-8 col-lg-8">
+          <div className="col-md-8 col-lg-4">
             <ul>
               <li>Hétfő-Péntek: 8-17 óráig</li>
               <li>Szombat: 8-13 óráig</li>
               <li>Vasárnap: 9-12 óráig</li>
             </ul>
           </div>
-          <div className="col-md-4 col-lg-4">
+          <div className="col-md-4 col-lg-2">
             <h3>Kapcsolat:</h3>
           </div>
-          <div className="col-md-8 col-lg-8">
+          <div className="col-md-8 col-lg-4">
             <ul>
               <li>06-30/111-1111</li>
               <li>06-70/111-1111</li>
