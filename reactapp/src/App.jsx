e0b23ecfca5fec1cb8da76d5025d64eb0509a78e
MrMancs <marcell.szatmari2007@gmail.com>
2025-11-20 08:38:38 +0100

    Formatting

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="nyito">
+        <header>
+          <img src="./sunflower.jpg" alt="fa" id="logo" />
+          <h1>Nevenincs Bt.</h1>
+          <h2>Vetőmagok - Mindenféle, minden mennyiségben</h2>
+        </header>
+        <main>
+          <a href="aruk.html">Válasszon vetőmagjainkból!</a>
+        </main>
+        <footer class="container">
+          <div class="row">
+            <div class="col-md-4 col-lg-4">
+              <h3>Nyitvatartás:</h3>
+            </div>
+            <div class="col-md-8 col-lg-8">
+              <ul>
+                <li>Hétfő-Péntek: 8-17 óráig</li>
+                <li>Szombat: 8-13 óráig</li>
+                <li>Vasárnap: 9-12 óráig</li>
+              </ul>
+            </div>
+            <div class="col-md-4 col-lg-4">
+              <h3>Kapcsolat:</h3>
+            </div>
+            <div class="col-md-8 col-lg-8">
+              <ul>
+                <li>06-30/111-1111</li>
+                <li>06-70/111-1111</li>
+                <li>nevenincsbt@gmail.com</li>
+              </ul>
+            </div>
+          </div>
+        </footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
-
-export default App
